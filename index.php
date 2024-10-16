5df1a3ec7249c9fe33721f61eb5b4ecd71d63a5a
MateuszKale <mateuszkaleta284@gmail.com>
2024-10-16 21:37:32 +0200

    types

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 
-// test komentarza liniowego 
+//Żeby zadeklarowac zmienną musimy uzyć znaku $
 
-/* test 
-komentarza
-blokowego
+$name_new =
+
+/*
+Typy zmiennych w php
+integer - liczby całkowite (dodatnie i ujemne)
+float - liczby rzeczywiste (dodatnie i ujemne )
+String - łancuch tekstowy
+Boolean(bool) - przyjmuje wartość true albo falste
+array - Tablica tablice mozemy definiowac za pomoca []; albo array();
+object - obiekt 
+null - zachodzi w przypadku gdy zmiennej nie przypiszemy wartosci 
+resource - przechowuje odwołanie do zasobu zewnetrzengo np bazy danych 
 */
 
-echo " <h1> Witam </h1>";
+$coins = 50;
+$balance = 100;
+$name_new = $coins + $balance;
+echo $name_new;
+echo  "<h1> Witam </h1>";
 echo ('<h3>Inny przyklad uzycia echo </h3>');
